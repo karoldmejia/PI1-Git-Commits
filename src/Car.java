64dc1a19f381b0c2ecede95fd9882657efc2ab2b
David Montaño <cuatro04sr@gmail.com>
2023-02-15 21:52:46 -0500

    Resuelto conflicto en Car.java y añadido el método toString

--- src/Car.java
+++ src/Car.java
@@ -7,4 +7,22 @@ public class Car {
         this.wheels = wheels;
         this.cylinders = cylinders;
     }
+
+    public boolean isWheels() {
+        return wheels;
+    }
+
+    public void setCylinders(int cylinders) {
+        this.cylinders = cylinders;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    @Override
+    public String toString() {
+        return "Wheels: " + wheels + ", Cylinders: " + cylinders + "\n";
+    }
 }
+
